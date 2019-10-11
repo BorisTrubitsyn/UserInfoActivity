76260108b6f1491f2931f867ebde285d1b54c635
Bertolda <boris.sen4ikov1994@yandex.ru>
2019-10-12 01:22:13 +0200

    add_findViewById

---
+++ app/src/main/java/com/example/userinfoactivity/UserInfoActivity.java
@@ -0,0 +1,38 @@
+package com.example.userinfoactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.squareup.picasso.Picasso;
+
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    private ImageView userImageView;
+    private TextView nameTextView;
+    private TextView nickTextView;
+    private TextView descriptionTextView;
+    private TextView locationTextView;
+    private TextView followingCountTextView;
+    private TextView followersCountTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        userImageView = findViewById(R.id.user_image_view);
+        nameTextView = findViewById(R.id.user_name_text_view);
+        nickTextView = findViewById(R.id.user_nick_text_view);
+        descriptionTextView = findViewById(R.id.user_description_text_view);
+        locationTextView = findViewById(R.id.user_location_text_view);
+        followersCountTextView = findViewById(R.id.followers_count_text_view);
+        followingCountTextView = findViewById(R.id.following_count_text_view);
+
+
+    }
+}
