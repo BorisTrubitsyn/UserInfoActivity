6830d6365678e79d4aa6a450ac4cca81a615df8e
Bertolda <boris.sen4ikov1994@yandex.ru>
2019-10-27 03:40:52 +0200

    Finish Twitter First Module

--- app/src/main/java/com/example/userinfoactivity/activity/SearchUsersActivity.java
+++ app/src/main/java/com/example/userinfoactivity/activity/SearchUsersActivity.java
@@ -2,8 +2,16 @@ package com.example.userinfoactivity.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,44 +25,74 @@ import java.util.Collection;
 public class SearchUsersActivity extends AppCompatActivity {
     private RecyclerView usersRecyclerView;
     private UsersAdapter usersAdapter;
-
+    private Toolbar toolbar;
+    private EditText queryEditText;
+    private Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_users);
         initRecyclerView();
-        searchUsers();
 
+        toolbar = findViewById(R.id.toolbar);
+        queryEditText = toolbar.findViewById(R.id.query_edit_text);
+        searchButton = toolbar.findViewById(R.id.search_button);
 
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchUsers();
+            }
+        });
+
+        queryEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    searchUsers();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     private void initRecyclerView() {
         usersRecyclerView = findViewById(R.id.users_recycler_view);
         usersRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-
         UsersAdapter.OnUserClickListener onUserClickListener = new UsersAdapter.OnUserClickListener() {
             @Override
             public void onUserClick(User user) {
                 Intent intent = new Intent(SearchUsersActivity.this, UserInfoActivity.class);
+                intent.putExtra(UserInfoActivity.USER_ID, user.getId());
                 startActivity(intent);
             }
         };
         usersAdapter = new UsersAdapter(onUserClickListener);
         usersRecyclerView.setAdapter(usersAdapter);
-
-
     }
-
 
     private void searchUsers() {
         Collection<User> users = getUsers();
+        usersAdapter.clearItems();
         usersAdapter.setItems(users);
-
     }
-
 
     private Collection<User> getUsers() {
         return Arrays.asList(
@@ -70,7 +108,6 @@ public class SearchUsersActivity extends AppCompatActivity {
                 ),
 
                 new User(
-
                         44196397L,
                         "https://pbs.twimg.com/profile_images/782474226020200448/zDo-gAo0_400x400.jpg",
                         "Elon Musk",
@@ -82,5 +119,4 @@ public class SearchUsersActivity extends AppCompatActivity {
                 )
         );
     }
-
 }
